7c30f4b1ec54f98b32005e5e75213b6cffb3b0f8
vi mark <webvimark@gmail.com>
2015-05-24 20:01:54 +0200

    dev db

---
+++ config/dev/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'class'             => 'yii\db\Connection',
+	'charset'           => 'utf8',
+	'enableSchemaCache' => true,
+	'dsn'               => 'mysql:host=localhost;dbname=ybc',
+	'username'          => '',
+	'password'          => '',
+];
